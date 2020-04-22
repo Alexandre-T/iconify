34e7e25e475fa2ad2dbfb4255d5ff82baff2090c
Alexandre-T <alexandre.tranchant@gmail.com>
2020-04-22 22:24:37 +0200

    Encore added and configured

--- src/Controller/VerificationController.php
+++ src/Controller/VerificationController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Verification controller
+ */
 class VerificationController extends AbstractController
 {
     /**
