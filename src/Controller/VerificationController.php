6f737d8bb405c208764838413aa880600f3f50a8
Alexandre-T <alexandre.tranchant@gmail.com>
2020-04-22 21:34:34 +0200

    Controller to verify created icons

---
+++ src/Controller/VerificationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VerificationController extends AbstractController
+{
+    /**
+     * @Route("/verification", name="verification")
+     */
+    public function index()
+    {
+        $finder = new Finder();
+        $files = $finder->name('*.png')->in(__DIR__ . '/../../public/output');
+        $icons = [];
+
+        foreach ($files as $file) {
+            $icons[] = basename($file);
+        }
+
+        return $this->render('verification/index.html.twig', [
+            'icons' => $icons,
+        ]);
+    }
+}
